79aed84a1a8001a637f4514e32a97aeccfac3e64
Ahmed Ansari <ahmedansari@eduroam-int-dhcp-97-173-26.ucl.ac.uk>
2023-02-25 16:25:04 +0000

    added linkbox, header

--- diagnosis-app/src/App.js
+++ diagnosis-app/src/App.js
@@ -1,10 +1,14 @@
 import Askgpt_page from "./pages/Askgpt_page"
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [Response, setResponse] = useState(null)
   return (
     <div className="App">
+      <div style={{color:"black"}}>Nav</div>
       <Askgpt_page/>
+      
     </div>
   );
 }
