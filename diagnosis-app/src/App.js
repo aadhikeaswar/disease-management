68d6b8db1aee0dee6db059e7f350c8db9c189666
Aykan125 <zcabaak@ucl.ac.uk>
2023-02-25 19:26:46 +0000

    my second commit

--- diagnosis-app/src/App.js
+++ diagnosis-app/src/App.js
@@ -1,18 +1,43 @@
-import Askgpt_page from "./pages/Askgpt_page"
-import Header from "./components/HEADER/Header";
-import { useState } from "react";
+// import Askgpt_page from "./pages/Askgpt_page"
+// import Header from "./components/HEADER/Header";
+// import { useState } from "react";
+// import './App.css';
+
+// function App() {
+//   const [Response, setResponse] = useState(null)
+//   return (
+//     <div className="App">
+    
+//       <Header/>
+//       <Askgpt_page setResponse={e=>setResponse(e)}/>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import {useState} from 'react';
+import Calendar from 'react-calendar'; 
 import './App.css';
 
 function App() {
-  const [Response, setResponse] = useState(null)
-  return (
-    <div className="App">
-    
-      <Header/>
-      <Askgpt_page setResponse={e=>setResponse(e)}/>
-      
-    </div>
-  );
+ const [date, setDate] = useState(new Date())
+
+
+
+return (
+ <div className="app">
+   <h1 className="header">React Calendar</h1>
+   <div className="calendar-container">
+     <Calendar onChange={setDate} value={date}/>
+   </div>
+   <div className="text-center">
+      Selected date: {date.toDateString()}
+   </div>
+ </div>
+  )
+
 }
 
 export default App;
