2de70dd38d279f4b1b515d1a24b079b60c1d7f11
Mohammed Fahad Tabrez Sheikh <69897389+ProgrammerFS@users.noreply.github.com>
2023-02-26 11:26:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
-
+import csv
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 @app.route("/", methods=("GET", "POST"))
-def index():
+def index(): 
     if request.method == "POST":
         symptoms = request.form["symptoms"]
         response = openai.Completion.create(
@@ -23,6 +24,34 @@ def index():
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
+
+# @app.route("/login")
+# def login():
+#     return "Login"
+
+# @app.route("/register")
+# def register():
+#     return "Register"
+@app.route("/add-meds", methods=("GET", "POST"))
+def add_meds():
+    if request.method == "POST":
+        r = request.form
+        med = r.get("medication")
+        dosage = r.get("dosage")
+        frequency = r.get("frequency")
+        startDate = r.get("startDate")
+        endDate = r.get("endDate")
+        with open("./database.csv", "a") as f:
+            writer= csv.writer(f, delimiter=",")
+            writer.writerow([med, frequency, startDate, endDate])
+        return "Prescription added"
+    else:
+        return "Add prescription first"
+
+@app.route("/test")
+@cross_origin()
+def test():
+    return "Test"
 
 
 def generate_prompt(symptoms):
